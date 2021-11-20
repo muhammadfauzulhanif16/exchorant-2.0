94c8a99750f8f95652329ab095c432149f9d2275
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-21 00:33:12 +0700

    create navbar styled

---
+++ src/components/common/NavBar/Styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Nav } from "react-bootstrap";
+
+const { Link } = Nav;
+
+export const LinkStyled = styled(Link)`
+  font-weight: 600;
+  color: #171923 !important;
+`;
