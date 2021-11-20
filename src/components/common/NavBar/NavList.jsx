0f89911c9c16e7dda447e256a21a1fe431b9f429
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-11-21 00:32:51 +0700

    create navlist

---
+++ src/components/common/NavBar/NavList.jsx
@@ -0,0 +1,7 @@
+export const NavList = [
+  { title: "Home", to: "home" },
+  { title: "Dish", to: "dish" },
+  { title: "About", to: "about" },
+  { title: "Team", to: "team" },
+  { title: "Contact", to: "contact" },
+];
