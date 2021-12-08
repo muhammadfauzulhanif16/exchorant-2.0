7819f31b200b434acf06d9c7088a789f12b4d0a1
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 05:03:41 +0700

    create nav list for footer

---
+++ src/components/Footer/NavList.jsx
@@ -0,0 +1,23 @@
+export const NavList = [
+  {
+    title: "Excellent Exho Project",
+    subLink: [
+      { title: "Impact Sport", link: "https://impactsport.netlify.app" },
+      { title: "Exchorant", link: "https://exchorant-2.netlify.app" },
+      { title: "Exo Trans", link: "https://exo-travel.netlify.app" },
+      { title: "Exchoflix", link: "" },
+      { title: "Pokemasterr", link: "" },
+      { title: "Echo-19", link: "" },
+      { title: "Exch-Stack", link: "" },
+      { title: "Si-NemAH", link: "" },
+    ],
+  },
+  {
+    title: "Legal",
+    subLink: [
+      { title: "Claim", link: "" },
+      { title: "Privacy", link: "" },
+      { title: "Terms", link: "" },
+    ],
+  },
+];
