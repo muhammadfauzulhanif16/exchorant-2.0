f2d9dee9e2647b691803cdaeb1f2c198a000f656
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 02:27:03 +0700

    feature list

---
+++ src/components/Section/Home/Feature/FeatureList.jsx
@@ -0,0 +1,32 @@
+export const FeatureList = [
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689674.png",
+    title: "Open 24 Hours",
+    content: "Ready to serve orders whenever & wherever you're.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689682.png",
+    title: "High-End Taste",
+    content: "High taste with selected ingredients & maintained hygiene.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689686.png",
+    title: "Affordable Prices",
+    content: "Affordable & friendly to everyone's wallet.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689701.png",
+    title: "Free Delivery",
+    content: "No shipping costs & fast delivery.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689759.png",
+    title: "Easy Order",
+    content: "Easy ordering via app with one click.",
+  },
+  {
+    image: "https://cdn-icons-png.flaticon.com/512/4689/4689908.png",
+    title: "Payment",
+    content: "Easy & secure payment with any method.",
+  },
+];
