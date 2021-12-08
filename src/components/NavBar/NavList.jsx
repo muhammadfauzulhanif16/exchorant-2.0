f19acdaf10fdbc2a949809943ae5b01599546639
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 02:22:30 +0700

    nav list for navbar

---
+++ src/components/NavBar/NavList.jsx
@@ -0,0 +1,9 @@
+export const NavList = [
+  { title: "Home", to: "home" },
+  { title: "About", to: "about" },
+  { title: "Dish", to: "dish" },
+  { title: "Teams", to: "teams" },
+  { title: "Contact", to: "contact" },
+  { title: "Sign In", to: "Sign In", isTrue: true },
+  { title: "Sign Up", to: "Sign Up" },
+];
