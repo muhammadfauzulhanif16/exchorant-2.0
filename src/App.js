3e30890a02d6960050b803d6912cd358698f7ee5
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 02:27:26 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-// import "./App.css";
-import { Home } from "./components";
+import { Home } from "./components/pages/Home";
+import { NavBar } from "./components/NavBar";
+import React from "react";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="bg-white user-select-none">
+      <NavBar />
       <Home />
-    </>
+      <Footer />
+    </div>
   );
 }
 
