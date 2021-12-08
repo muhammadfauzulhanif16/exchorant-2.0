21ca1fc027d8aa58dd615379f5d6f921f8680196
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 02:25:23 +0700

    create about section

---
+++ src/components/Section/Home/About/index.jsx
@@ -0,0 +1,16 @@
+import { Section } from "../../../common/Section";
+
+export const About = () => {
+  return (
+    <Section
+      title="ABOUT"
+      shortDesc="asdasdasdasdsad"
+      longDesc="sadasdasdasasdds"
+    >
+      <p>
+        The Excho Restaurant website is a platform that provides various types
+        of dishes such as food and drinks
+      </p>
+    </Section>
+  );
+};
