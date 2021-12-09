aad4e773a3983e2f813806a83df555f5bcb79db9
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-09 07:47:07 +0700

    clean up code hero section

--- src/components/Section/Home/Hero/index.jsx
+++ src/components/Section/Home/Hero/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Carousel } from "../../../common/Carousel";
-import "./style.scss";
-import { Button, Row, Col, Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 
 export const Hero = () => {
   return (
     <Container fluid className="container-md pe-0">
-      <Row className="vh-60">
-        <Col className="d-flex flex-column justify-content-center">
+      <div className="d-md-flex flex-md-row-reverse">
+        <div className="d-flex justify-content-end align-items-center col mb-5">
+          <Carousel />
+        </div>
+
+        <div className="d-flex flex-column justify-content-center col">
           <h1 className="text-dark-gray-5b5555">
             <span className="text-red-e5646e">Delicious dishes</span> from all
             over the world
@@ -22,12 +25,8 @@ export const Hero = () => {
           <Button size="lg" className="shadow-lg col-6 py-3 fs-6 border-0">
             Looking for a Dish
           </Button>
-        </Col>
-
-        <Col className="d-flex justify-content-end align-items-center">
-          <Carousel />
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Container>
   );
 };
