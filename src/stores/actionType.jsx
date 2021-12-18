9f471123528a6e26a5bf842512dd562fb6d72a07
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-18 11:20:56 +0700

    create dish action type

---
+++ src/stores/actionType.jsx
@@ -0,0 +1,7 @@
+export const DishActType = {
+  getDish: "GET_DISH",
+  getDishSummary: "GET_DISH_SUMMARY",
+  getDishDetail: "GET_DISH_DETAIL",
+  addDishFav: "ADD_DISH_FAV",
+  removeDishFav: "REMOVE_DISH_FAV",
+};
