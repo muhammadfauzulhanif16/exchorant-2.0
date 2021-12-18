e2ae5d11fdf8fadc6cd828552aff7c4b644ff715
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-18 11:25:52 +0700

    Image list dummy

---
+++ src/components/common/Carousel/ImageList.tsx
@@ -0,0 +1,32 @@
+export const ImageList = [
+  {
+    image:
+      "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "1",
+    id: 1,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "2",
+    id: 2,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    title: "3",
+    id: 3,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1142950/pexels-photo-1142950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "4",
+    id: 4,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "5",
+    id: 5,
+  },
+];
