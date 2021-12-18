ca024db2dbb3d978aed16273f008eaf7a9c20425
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2021-12-18 11:26:46 +0700

    create index dish act

---
+++ src/stores/actions/index.jsx
@@ -0,0 +1,5 @@
+export { DishAct } from "./dishAct";
+export { DishSummaryAct } from "./dishAct";
+export { DishDetailAct } from "./dishAct";
+export { AddDishFavAct } from "./dishAct";
+export { RemoveDishFavAct } from "./dishAct";
